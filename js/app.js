f01bc95d5e85173cf7b793b5349e7c72b6df0281
Melanny <melanny.lima@outlook.com>
2022-07-16 16:05:07 -0400

    created instruction screen

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
 console.log("Beach Invaders!");
+
+// This game has 3 rounds and has to take the input of 2 players
+// There has to be several screens: start, instructions, play, winner screen
+
+// start to instructions transition
+
+const instructions = () => {
+  // Remove start screen
+  const startScreen = document.getElementById("toStart");
+  startScreen.remove();
+
+  // create title element for the instruction screen
+  let screenH = document.createElement("H1");
+  // Add the actual title to the title element
+  screenH.innerHTML = "Instructions:"
+
+  // create unordered list
+  const instList = document.createElement("UL")
+  // create list items
+  const firstL = document.createElement("LI")
+  firstL.innerHTML = "Example"
+  const secL = document.createElement("LI")
+  secL.innerHTML = "Example step 2"
+  // append list items to unordered list
+  instList.appendChild(firstL)
+  instList.appendChild(secL)
+
+  // get the instruction screen
+  const inst = document.getElementById("inst")
+
+  // attach the title to the instruction screen
+  inst.appendChild(screenH)
+  // attach unordered list to instruction screen
+  inst.appendChild(instList)
+}
+
+const startButton = document.getElementById("start")
+startButton.addEventListener("click", instructions)
