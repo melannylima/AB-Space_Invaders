ae0b6b923467391363b1343df997aa7ac8c27ba5
Melanny <melanny.lima@outlook.com>
2022-07-20 20:18:09 -0400

    takes name input for players

--- js/app.js
+++ js/app.js
@@ -53,7 +53,49 @@ const letsPlay = () => {
   // remove instructions screen
   const inst = document.getElementById("inst")
   inst.remove()
-  console.log(play);
+  //change screen
+  const back = document.getElementById("gamezone")
+  back.setAttribute("id", "level")
+
+  // receive name for players 1 & 2
+  const player1 = document.createElement("INPUT")
+  const playerb1 = document.createElement("BUTTON")
+  const playZone = document.getElementById("play")
+  player1.setAttribute("type", "text")
+  playerb1.setAttribute("id", "pb")
+  player1.setAttribute("placeholder", "Player 1 Name")
+  playerb1.innerHTML = "submit"
+  player1.setAttribute("id", "p1")
+  player1.setAttribute("class", "p1")
+  playZone.appendChild(player1)
+  playZone.appendChild(playerb1)
+  playerb1.addEventListener("click", player1Values)
+
+  const player2 = document.createElement("INPUT")
+  const playerb2 = document.createElement("BUTTON")
+  player2.setAttribute("type", "text")
+  playerb2.setAttribute("id", "pb")
+  player2.setAttribute("placeholder", "Player 2 Name")
+  playerb2.innerHTML = "submit"
+  player2.setAttribute("id", "p2")
+  player2.setAttribute("class", "p2")
+  playZone.appendChild(player2)
+  playZone.appendChild(playerb2)
+
+  playerb2.addEventListener("click", player2Values)
+  // demonstrate score for players 1 & 2
+  // create players and allow them to move and "shoot"
+  // impact
+}
+
+const player1Values = () => {
+  const pName1 = document.getElementById("p1").value;
+  console.log(pName1);
+}
+
+const player2Values = () => {
+  const pName2 = document.getElementById("p2").value;
+  console.log(pName2);
 }
 
 const startButton = document.getElementById("start")
